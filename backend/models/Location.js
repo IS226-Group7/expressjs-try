f07b2cbb3d60e2831d6c5c3e805966f20f0252ca
MARLODEL SERVO <mrservo@up.edu.ph>
2026-04-25 04:11:18 +0000

    deleted unused tables

--- backend/models/Location.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
-
-const Location = sequelize.define('Location', {
-  name: { type: DataTypes.STRING, allowNull: false }, // e.g., "Warehouse A"
-  room: { type: DataTypes.STRING }
-});
-
-export default Location;
