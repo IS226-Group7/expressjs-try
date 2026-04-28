30c5d783e98b0d9dcbe782d6b4a26441edad3bb1
MARLODEL SERVO <mrservo@up.edu.ph>
2026-04-28 08:00:43 +0000

    remove console logs in middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,17 +4,14 @@ export const verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
 
   if (!token) {
-    console.log("no token");
     return res.status(403).json({ message: "No security token provided." });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // This contains the user ID and role
-    console.log("user has verified token");
     next();
   } catch (err) {
-    console.log("invalid token");
     return res.status(401).json({ message: "Invalid or expired token." });
   }
 };
